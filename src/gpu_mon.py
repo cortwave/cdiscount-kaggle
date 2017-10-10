5fc414877405fe70dd050eb02acd89b586e377ae
Dmitry Pranchuk <d.pranchuk@gmail.com>
2017-10-10 03:00:04 +0300

    gpu monitor has file and interval options and works with diff # of gpus now

--- src/gpu_mon.py
+++ src/gpu_mon.py
@@ -1,18 +1,29 @@
 import subprocess
 import json
-from time import time
+from time import time, sleep
+from fire import Fire
 
-with open('/home/arseny/gpu.log', 'a') as out:
-    while True:
-        s = subprocess.run(['nvidia-smi', '--format=csv', '--query-gpu=power.draw,utilization.gpu,temperature.gpu'],
-                           stdout=subprocess.PIPE)
-        head, gpu0, gpu1, _ = s.stdout.decode().split('\n')
+FILE_NAME = './gpu.log'
 
-        for i, gpu in enumerate((gpu0, gpu1)):
-            power, util, temp = gpu.split(', ')
-            out.write(json.dumps({'power': float(power.split(' ')[0]),
-                                  'util': float(util.split(' ')[0]),
-                                  'temp': float(temp.split(' ')[0]),
-                                  'time': str(time()),
-                                  'gpu': i,
-                                  }) + '\n')
+
+def write_log(file_name=FILE_NAME, interval=0):
+    with open(file_name, 'a+') as out:
+        while True:
+            s = subprocess.run(['nvidia-smi', '--format=csv', '--query-gpu=power.draw,utilization.gpu,temperature.gpu'],
+                               stdout=subprocess.PIPE)
+            gpus = s.stdout.decode().split('\n')[1:-1]
+            for i, gpu in enumerate(gpus):
+                power, util, temp = gpu.split(', ')
+                out.write(json.dumps({'power': float(power.split(' ')[0]),
+                                      'util': float(util.split(' ')[0]),
+                                      'temp': float(temp.split(' ')[0]),
+                                      'time': str(time()),
+                                      'gpu': i,
+                                      }) + '\n')
+                out.flush()
+            if interval != 0:
+                sleep(interval)
+
+
+if __name__ == '__main__':
+    Fire(write_log)
