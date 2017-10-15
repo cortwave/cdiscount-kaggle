342ecf09c539bfac1924ade13dea2206709175d3
Dmitry Pranchuk <d.pranchuk@gmail.com>
2017-10-15 15:23:57 +0300

    resnet 50, 101, 152

--- src/train_resnet.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-for i in {0..4};
-  do python3 model.py train --architecture=resnet50 --fold=$i --lr=0.0001 --batch-size=512 --iter-size=4 --epochs=200 --epoch-size=200000 --validation-size=100000;
-done
